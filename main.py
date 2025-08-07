78e3b91c2cea9857f224e29f2ac1c78f7c1d73c1
Leo.C <z.chui1@universityofgalway.ie>
2025-08-07 15:01:29 +0100

    Refine simulation logic

--- main.py
+++ main.py
@@ -1,30 +1,139 @@
+import time
+import json
+from collections import OrderedDict
+
+import energy_tracker
 import flwr as fl
+from flwr.common import Metrics, Context, Parameters, ndarrays_to_parameters
+from flwr.client import ClientApp
+from flwr.server import ServerApp, ServerConfig, ServerAppComponents
+from flwr.server.strategy import FedAvg
+from flwr.simulation import run_simulation
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+
+
 from client import IoTClient
 from dataset import preprocessing
+from server import FedAvgCustom, get_evaluate_fn
 from model.architecture import DNN, CNN
 from model.training import train_model, test_model
 from model.explainability import explain_with_shap, explain_with_captum
 
+# hardcoding input_dim to initiate net before creating clients
+# so that server can access it for global model initialization
+# used for eval purposes
+train_loader = torch.load(
+    "train_loader.pth", map_location="cuda:0", weights_only=False)
+test_loader = torch.load(
+    "test_loader.pth",  map_location="cuda:0", weights_only=False)
+input_dim = train_loader.dataset[0][0].shape[0]
+num_rounds = 3
+num_clients = 5
+iid = True
 
-def client_fn(cid):
-    return IoTClient(cid=int(cid)).to_client()
+
+def client_fn(context: Context):
+    # cid = int(context.node_id)
+    cid = int(context.node_config["partition-id"])
+    data_path = f'dataset/edge-iiotset/partitions_{"iid" if iid else "non-iid"}/{num_clients}_clients/client_{cid+1}.csv'
+
+    df = pd.read_csv(data_path, encoding='utf-8', low_memory=False)
+    X = df.drop(columns=["Attack_label"])
+    y = df["Attack_label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, stratify=y, random_state=42
+    )
+
+    train_loader = preprocessing.tensorise_and_wrap(X_train, y_train)
+    test_loader = preprocessing.tensorise_and_wrap(X_test, y_test)
+
+    return IoTClient(cid=cid, train_loader=train_loader, test_loader=test_loader, dp=True).to_client()
 
 
-def main(federate=True, federation_rounds: int = 5):
+def server_fn(context: Context):
+    model = CNN(input_dim=input_dim)
+    params = [val.cpu().numpy() for _, val in model.state_dict().items()]
+    global_model_init = ndarrays_to_parameters(params)
+    config = ServerConfig(num_rounds=num_rounds)
+    strategy = FedAvgCustom(
+        file_name="fedavg",
+        num_rounds=num_rounds,
+        fraction_fit=1.0,  # Sample 100% of available clients for training
+        fraction_evaluate=0.5,  # Sample 50% of available clients for evaluation
+        initial_parameters=global_model_init,  # initialised global model
+        evaluate_fn=get_evaluate_fn(
+            testloader=test_loader, input_dim=input_dim),
+    )
+
+    return ServerAppComponents(strategy=strategy, config=config)
+
+
+def main(federate=True):
     if federate:
-        fl.simulation.start_simulation(
+        assert num_clients in [3, 5, 10]
+        """fl.simulation.start_simulation(
             client_fn=client_fn,
             num_clients=5,
             config=fl.server.ServerConfig(num_rounds=federation_rounds)
+        )"""
+        server = ServerApp(server_fn=server_fn)
+        client = ClientApp(client_fn=client_fn)
+
+        run_simulation(
+            server_app=server,
+            client_app=client,
+            num_supernodes=num_clients
         )
     else:
         # run centralised pipeline
-        train_loader, test_loader, _ = preprocessing.preprocess()
-        model = DNN(input_dim=train_loader.dataset[0][0].shape[0])
-        train_model(model, train_loader, epochs=5, dp=True)
-        loss, acc = test_model(model, test_loader)
-        print(f"Loss: {loss} | Accuracy: {acc}")
+        # train_loader, test_loader, _ = preprocessing.preprocess()
+        global train_loader
+        global test_loader
+
+        """
+        model = CNN(input_dim=train_loader.dataset[0][0].shape[0])
+        train_model(model, train_loader, epochs=5, dp=False)
+        loss, metrics = test_model(
+            model, test_loader, global_model=True) 
+        print(f"Loss: {loss} | Metrics: {metrics}")
+        # write metrics to output/centralised/{file_name}.csv
+        metrics_df = pd.DataFrame.from_dict(metrics, orient="index").T
+        metrics_df.to_csv(f"output/centralised/baseline_dnn.csv")
+        """
+
+        # evaluate different DP noise levels
+        for noise in np.arange(0, 1, 0.25):
+            model = CNN(input_dim=train_loader.dataset[0][0].shape[0])
+            train_model(model, train_loader, epochs=5,
+                        dp=True, noise_multiplier=noise)
+            loss, metrics = test_model(model, test_loader, global_model=True)
+            print(f"Loss: {loss} | Metrics: {metrics}")
+            # write metrics to output/centralised/{file_name}.csv
+            metrics_df = pd.DataFrame.from_dict(metrics, orient="index").T
+            metrics_df.to_csv(f"output/centralised/noise_{noise}.csv")
+
+        # explain model
+        explain_with_shap(model, test_loader)
+        explain_with_captum(model, test_loader)
 
 
 if __name__ == "__main__":
+    # experiment setup:
+    """
+    | Model Type     | DP  | FL  | XAI | Notes                           |
+    | -------------- | --- | --- | --- | -----------------------------   |
+    | Centralized    | No  | No  | No  | Baseline                        |
+    | Centralized    | Yes | No  | No  | DP impact                       |
+    | Federated      | No  | Yes | No  | FL overhead vs. central         |
+    | Federated      | Yes | Yes | No  | DP + FL performance tradeoff    |
+    | Federated      | Yes | Yes | Yes | XAI interpretability snapshot   |
+    | (Optional) Any | Any | Any | Yes | XAI interpretability comparison |
+    """
+    main(federate=False)
     main(federate=True)
